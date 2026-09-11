3420d198f97a33131f8a1a9f44d479e97977c95c
Thitwut Khrutchuen <khanun.krootchuen@gmail.com>
2026-09-11 17:39:48 +0700

    mod scheduler & app.py

--- web/app.py
+++ web/app.py
@@ -40,7 +40,7 @@ def delete_router(id):
 def router_detail(ip):
     # Retrieve the last 3 interface statuses for the given IP
     recent_statuses = list(
-        db.interface_status.find({"router_ip": ip}).sort("timestamp", -1).limit(3)
+        db.interface_status.find({"router_ip": ip}).sort("timestamp", -1).limit(5)
     )
 
     return render_template("router_detail.html", ip=ip, statuses=recent_statuses)
